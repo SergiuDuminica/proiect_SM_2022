533759c2242c2ae5725f7fe8d6f334c49e1670ae
Sergiu Duminica <ionel-sergiu.duminica@student.tuiasi.ro>
2022-06-02 12:44:24 +0300

    Commit final

--- ultrasunet.py
+++ ultrasunet.py
@@ -2,9 +2,13 @@ import RPi.GPIO as GPIO
 import time
 import smtplib
 import sys
+import os
 
 from datetime import datetime
 from time import strftime, localtime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 echoPIN = 15
 triggerPIN = 14
@@ -54,22 +58,31 @@ def distance ():
 	return distance
 
 server=smtplib.SMTP('smtp.gmail.com',587)
+server.ehlo()
 server.starttls()
 server.login("labsm2022@gmail.com","tudorsisergiu")
 
 try:
 	while True:
-		print (" Distance: " + str(distance())+ "   ", end='\r')
+		print ("Distance: " + str(distance()), end='\r\n')
 		if int(distance()) < 50:
 			print('A fost detectata miscare.')
 			ora=datetime.now()
 			ora_curenta=ora.strftime("%H:%M")
-			msg="From: Sistem de alarma antiefractie <labsm2022@gmail.com> \r\n"
-			msg+="To: <tudorsergiu12@gmail.com> \r\n"
-			msg+="Subject: Miscare neautorizata detectata! \r\n"
-			msg+="\r\n"
-			msg+="A fost detectata miscare la ora " + ora_curenta + ".\nIndividul se afla la distanta de " + str(distance()) + " cm fata de senzor.\nAlarma a pornit."
-			server.sendmail("labsm2022@gmail.com", "tudorsergiu12@gmail.com", msg)
+			msg=MIMEMultipart()
+			msg['From']="Sistem de alarma antiefractie <labsm2022@gmail.com>"
+			msg['To']="<tudorsergiu12@gmail.com>"
+			msg['Subject']="Miscare neautorizata detectata!"
+			body="A fost detectata miscare la ora " + ora_curenta + ".\n"
+			body+="Individul se afla la distanta de " + str(distance()) + " cm fata de senzor.\n"
+			body+="Alarma a pornit.\n"
+			body+="In sectiunea de atasamente a acestui mail se regaseste o poza cu individul.\n"
+			msg.attach(MIMEText(body))
+			os.system('fswebcam --device /dev/video0 image.jpg')
+			imgattach = MIMEImage(open("image.jpg", 'rb').read(), 'jpg')
+			imgattach.add_header('Content-Disposition', 'attachment; filename="image.jpg"')
+			msg.attach(imgattach)
+			server.sendmail("labsm2022@gmail.com", "tudorsergiu12@gmail.com", msg.as_string())
 			print('Email-ul a fost trimis.')
 			GPIO.output(buzzerPIN, buzzerSTATE)
 			print('A pornit alarma.')
